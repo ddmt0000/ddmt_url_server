8d8dcc274d0c11bf50e8df03f09e06dfd10e7e7d
BuildTools <unconfigured@null.spigotmc.org>
2023-11-19 23:27:09 +0800

    mian

---
+++ conf/conf.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    'db' =>
+    array(
+        'mysql' =>
+        array(
+            'host' => 'localhost',
+            'user' => 'ddmt_urlsrver',
+            'password' => '000000',
+            'name' => 'ddmt_urlsrver',
+            'tablepre' => 'dus_', //Not yet
+            'charset' => 'utf8',
+        ),
+    ),
+    'sitename' => 'DDMT url server',
+    'lang' => 'zh-cn'
+);
--- route/mysql.php
+++ route/mysql.php
@@ -1,15 +1,24 @@
 <?php
-$servername = "localhost";
-$username = "ddmt_urlsrver";
-$password = "000000";
-$dbname = "ddmt_urlsrver";//链接创建
-$conn = new mysqli($servername, $username, $password, $dbname);//检测链接
+/*
+ * Copyright (C) ddmt.top
+ * 2023/11/19 23:14
+ */
+
+$config = include './conf/conf.php';
+$db_servername = $config['db']['mysql']['localhost'];
+$db_username = $config['db']['mysql']['user'];
+$db_password = $config['db']['mysql']['password'];
+$db_name = $config['db']['mysql']['name'];
+$site_name = $config['db']['sitename'];
+$conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
+
+//数据库连接部分
+
 if ($conn->connect_error) {
     die("连接失败: " . $conn->connect_error);
 }
-//天界记录
-function url_add($url_ip, $url_new, $url_orange)
-{
+//添加记录
+function url_add($url_ip, $url_new, $url_orange){
     global $conn;
     $url_orange = urlencode($url_orange);
     $submission_date = date("Y-m-d H:i:s"); // 当前日期时间
@@ -22,23 +31,23 @@ function url_add($url_ip, $url_new, $url_orange)
         return $sql . ";" . $conn->error;
     }
 }
+
 //通过new寻找原始url
-function url_find_orange($url_new)
-{
+function url_find_orange($url_new){
     global $conn;
-    // 创建查询语句
-    $sql = "SELECT url_orange FROM url_li WHERE url_new = '$url_new'";    // 执行查询语句
-    $result = $conn->query($sql);    if ($result->num_rows > 0) {
-        // 输出查询结果
+    $sql = "SELECT url_orange FROM url_li WHERE url_new = '$url_new'";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             return urldecode($row["url_orange"]);
         }
     } else {
         return null;
     }
-}//通过ip寻找原始url
-function url_find_ip($user_ip)
-{
+} 
+
+//通过ip寻找原始url
+function url_find_ip($user_ip){
     global $conn;
     $sql = "SELECT url_orange FROM url_li WHERE url_ip = '$user_ip'";
     $result = $conn->query($sql);
@@ -51,11 +60,11 @@ function url_find_ip($user_ip)
     } else {
         return array();
     }
-}//检查数据库是否存在该url
-function url_examine_orange($url_orange)
-{
+}
+
+//检查数据库是否存在该url
+function url_examine_orange($url_orange){
     global $conn;
-    // 创建查询语句
     $url_orange = urlencode($url_orange);
     $sql = "SELECT url_new FROM url_li WHERE url_orange = '$url_orange'";
     $result = $conn->query($sql);
@@ -67,13 +76,14 @@ function url_examine_orange($url_orange)
         return null;
     }
 }
+
 //统计总数
-function url_all()
-{
+function url_all(){
     global $conn;
     $sql = "SELECT COUNT(*) AS total FROM url_li";
     // 执行查询语句
-    $result = $conn->query($sql);    if ($result->num_rows > 0) {
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
         // 输出查询结果
         while ($row = $result->fetch_assoc()) {
             return $row["total"];
@@ -82,17 +92,19 @@ function url_all()
         return null;
     }
 }
+
 //获取一个随机code
-function generateRandomString($length)
-{
+function generateRandomString($length){
     $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $randomString = '';    for ($i = 0; $i < $length; $i++) {
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
         $randomString .= $characters[mt_rand(0, strlen($characters) - 1)];
-    }    return $randomString;
+    }
+    return $randomString;
 }
+
 //检查doce
-function find_RandomString($randomString)
-{
+function find_RandomString($randomString){
     $pattern = '/^[a-zA-Z0-9]{6}$/';
     return preg_match($pattern, $randomString);
 }
